32ac8006d275184389bd3d694728e2a08daf8a8e
Douglas <douglasbelinato@cvccorp.com.br>
2020-10-14 08:21:13 -0300

    Configuration of list users Route

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+// Configure Express to work with json sent in request body
+app.use(express.json());
+
+app.get("/users", (request, response) => {
+  return response.json({ message: "Hello World!" });
+});
+
 app.listen(3333);
