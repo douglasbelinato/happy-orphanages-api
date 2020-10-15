00e089d2831a109dbf9d26fd8e5abdf9613bc8a8
Douglas <douglasbelinato@cvccorp.com.br>
2020-10-15 13:42:53 -0300

    Working with views

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "./database/connection";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 
 // Configure Express to work with json sent in request body
 app.use(express.json());
 app.use(routes);
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.listen(3333);
