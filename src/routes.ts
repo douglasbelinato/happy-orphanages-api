f9991bb14898b5eeb15f9e45aa61fd658d7dfea4
Douglas <douglasbelinato@cvccorp.com.br>
2020-10-15 07:32:22 -0300

    Creating index and show routes in orphanages controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,10 @@ import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
 
+// Resouce name patterns most common for controllers in node / typescript:
+// index, show, create, update, delete
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
 routes.post("/orphanages", OrphanagesController.create);
 
 export default routes;
