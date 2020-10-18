0ad5681ecada8e1f04d9c5f069681217118bb51d
Douglas <douglasbelinato@cvccorp.com.br>
2020-10-18 11:12:04 -0300

    Creating exception handler

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import "express-async-errors";
 import "./database/connection";
 import routes from "./routes";
 import path from "path";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
@@ -11,5 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333);
